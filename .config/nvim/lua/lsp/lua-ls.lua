56c0ba77306b6abcca8f1afb655a7fd4050d6293
Piotr Karpiński <gorveenn@gmail.com>
2021-04-29 11:49:35 +0200

    Fix lua root detection

--- .config/nvim/lua/lsp/lua-ls.lua
+++ .config/nvim/lua/lsp/lua-ls.lua
@@ -1,10 +1,14 @@
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
 local sumneko_root_path = DATA_PATH .. "/lspinstall/lua"
 local sumneko_binary = sumneko_root_path .. "/sumneko-lua-language-server"
+local utils = require("lspconfig.util")
 
 require'lspconfig'.sumneko_lua.setup {
     cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
     on_attach = require'lsp'.common_on_attach,
+    root_dir = function(fname)
+        return vim.loop.cwd()
+    end,
     settings = {
         Lua = {
             runtime = {
